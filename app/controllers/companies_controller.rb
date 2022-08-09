cafc0e48c6f88de48cfb8ba2f9193077c4adc00b
Raphael Marsan <marsanraphael@gmail.com>
2022-08-09 12:20:00 +0200

    add basic logic to create lead with form

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -5,12 +5,27 @@ class CompaniesController < ApplicationController
 
   def show
     @company = Company.find(params[:id])
+    @lead = @company.lead
     @service_result = FetchInsurance.new(company: @company).call
   end
 
   def create
-    @company = Company.new(company_params)
-    @company.nacebel_codes = params['nacebel_codes']
+    @lead = Lead.create(
+      email: params['email'],
+      phone_number: params['phone_number'],
+      address: params['address'],
+      first_name: params['first_name'],
+      last_name: params['last_name']
+    )
+
+    @company = Company.new(
+      annual_revenue: params['annual_revenue'],
+      enterprise_number: params['enterprise_number'],
+      legal_name: params['legal_name'],
+      natural_person: params['natural_person'],
+      nacebel_codes: params['nacebel_codes'],
+      lead_id: @lead.id
+    )
 
     if @company.save
       redirect_to company_path(@company)
